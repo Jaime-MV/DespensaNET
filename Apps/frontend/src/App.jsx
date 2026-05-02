ad65e3072d92739c3630bc72feade98fe7bc1f40
Jaime-MV <jaime.a.matamoros@gmail.com>
2026-05-01 19:02:46 -0600

    Configuracion inicial del monorepo

---
+++ Apps/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>DespensaNet</h1>
+      <p>Bienvenido a la aplicación.</p>
+    </div>
+  );
+}
+
+export default App;
